732cd99bb3fd1df8e00cc1e3f43c1d2d80b21d1c
omaralhajj <omar-alhajj@hotmail.com>
2019-08-11 10:51:24 +0200

    no message

--- app/src/main/java/com/itpirates/leasingpartner/MainActivity.kt
+++ app/src/main/java/com/itpirates/leasingpartner/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.widget.EditText;
 import android.widget.TextView
 
@@ -19,56 +20,28 @@ class MainActivity : AppCompatActivity() {
         carPrice = findViewById(R.id.carPriceText)
         fee = findViewById(R.id.feeText)
         payout = findViewById(R.id.payoutText)
-
-        carPrice.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (!AreTextFieldsNull()) {
-                    CalculatePrice()
-                }
-            }
-        })
-
-        fee.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (!AreTextFieldsNull()) {
-                    CalculatePrice()
-                }
-            }
-        })
-
-        payout.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (!AreTextFieldsNull()) {
-                    CalculatePrice()
-                }
-            }
-        })
     }
 
     private fun AreTextFieldsNull(): Boolean {
         return carPrice.text.isNullOrBlank() || fee.text.isNullOrBlank() || payout.text.isNullOrBlank()
     }
 
-    fun CalculatePrice() {
-        val extraordinaryLeaseTV = findViewById<TextView>(R.id.extraordinaryTextView)
-        val monthlyLeaseTV = findViewById<TextView>(R.id.monthlyTextView)
-        val residualTV = findViewById<TextView>(R.id.residualTextView)
+    fun CalculatePrice(view: View) {
+        if (!AreTextFieldsNull()) {
+            val extraordinaryLeaseTV = findViewById<TextView>(R.id.extraordinaryTextView)
+            val monthlyLeaseTV = findViewById<TextView>(R.id.monthlyTextView)
+            val residualTV = findViewById<TextView>(R.id.residualTextView)
 
-        val carPrice = carPrice.text.toString().toDouble()
-        val fee = fee.text.toString().toDouble()
-        val payout = payout.text.toString().toDouble()
+            val carPrice = carPrice.text.toString().toDouble()
+            val fee = fee.text.toString().toDouble()
+            val payout = payout.text.toString().toDouble()
 
-        val extraordinaryLease = ExtraordinaryLease()
-        val monthlyLease = MonthlyLease()
+            val extraordinaryLease = ExtraordinaryLease()
+            val monthlyLease = MonthlyLease()
 
-        extraordinaryLeaseTV.text = extraordinaryLease.Calculate(carPrice, payout).toString()
-        monthlyLeaseTV.text = monthlyLease.Calculate(fee, payout, carPrice).toString()
-        residualTV.text = (carPrice * 7.5 * 0.8).toString()
+            extraordinaryLeaseTV.text = String.format("%.0f", extraordinaryLease.Calculate(carPrice, payout)) + " kr."
+            monthlyLeaseTV.text = String.format("%.0f", monthlyLease.Calculate(fee, payout, carPrice)) + " kr."
+            residualTV.text = String.format("%.0f", (carPrice * 7.5 * 0.8)) + " kr."
+        }
     }
 }
